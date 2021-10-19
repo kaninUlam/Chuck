5cf25697073182f86344f8cf69e27b4293e474f8
John Allen <valerio.allen24@gmail.com>
2021-10-19 14:47:13 +1300

    Camera Follow and Camera Shake
    Camera Follow and Camera Shake added
    has simple movement script
    Key Prefab ** replace sprite with key asset**

--- Assets/John/Scripts/keyStat.cs
+++ Assets/John/Scripts/keyStat.cs
@@ -8,12 +8,16 @@ public class keyStat : MonoBehaviour
     // name item as Key
     // sets a stats for if the player has a key or not
     public bool hasKey = false;
+
+    public cameraShake CameraShake;
+
     // sets hasKey to true when player collides with object that is named Key
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name.Contains("Key"))
         {
             hasKey = true;
+            StartCoroutine(CameraShake.Shake(.15f, .05f));
         }
     }
 }
