544b736af02527f68287d8c5472b880e65fb4330
John Allen <valerio.allen24@gmail.com>
2021-10-19 13:24:38 +1300

    Key and Door
    Key stat check on player if they have key or not.
    
    Key Item which destroys object on pickup
    
    Door trigger which checks if the player has key which opens the door. door still closed if player has no key

---
+++ Assets/John/Scripts/DoorTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    // reference for objects
+    GameObject player;
+    keyStat key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // player finds the game object of the player. ** Replace string to Chuck **
+        player = GameObject.Find("Player");
+        // gets the Component of the player that is the keystat script
+        key = player.GetComponent<keyStat>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // checks if the object is the player ** replace string  to chuck **
+        if (collision.gameObject.name.Contains("Player"))
+        {
+            // checks if the player has the key
+            if (key.hasKey == true)
+            {
+                Debug.Log("Door Open");
+                transform.position = transform.position + new Vector3(0, 10, 0);
+            }
+            if (key.hasKey == false)
+            {
+                Debug.Log("Door still close");
+            }
+        }
+       
+    }
+}
---
+++ Assets/John/Scripts/keyStat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyStat : MonoBehaviour
+{
+    // set collider2d on item and tick is trigger in collider
+    // name item as Key
+    // sets a stats for if the player has a key or not
+    public bool hasKey = false;
+    // sets hasKey to true when player collides with object that is named Key
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Key"))
+        {
+            hasKey = true;
+        }
+    }
+}
